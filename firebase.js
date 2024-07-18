5ef0c858e30315e5b38a70dd4ad38ccd3716f509
shuyu0714 <upgedc13@hotmail.com>
2024-07-18 17:43:17 +0800

    Change Firebase Variables

--- firebase.js
+++ firebase.js
@@ -6,14 +6,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDeTfL3kbvaTHNbxx86MKnmjnH8BWRDQk0",
-  authDomain: "ecosprint-punch.firebaseapp.com",
-  databaseURL: "https://ecosprint-punch-default-rtdb.firebaseio.com",
-  projectId: "ecosprint-punch",
-  storageBucket: "ecosprint-punch.appspot.com",
-  messagingSenderId: "932924685888",
-  appId: "1:932924685888:web:6a3ddcd195b676e065e9a5",
-  measurementId: "G-40FFT0D2XG"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
